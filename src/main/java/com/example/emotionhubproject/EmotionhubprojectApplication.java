f639cdbfa3c2b15d72fc7fa4d6ce085060c4bcdd
shs98ko <shs98ko@naver.com>
2026-05-22 22:18:01 +0900

    Project Setting

---
+++ src/main/java/com/example/emotionhubproject/EmotionhubprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.emotionhubproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionhubprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionhubprojectApplication.class, args);
+	}
+
+}
